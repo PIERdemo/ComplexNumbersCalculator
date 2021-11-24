e18df9928e6a4dd9399b506b1639fe6015c2fc4e
Merybria99 <maria.rosariabriglia99@gmail.com>
2021-11-24 13:18:01 +0100

    add methods sum and multiply to ArithmeticStackOperations

--- Calculator/src/main/java/it/unisa/se/calculator/model/ArithmeticStackOperations.java
+++ Calculator/src/main/java/it/unisa/se/calculator/model/ArithmeticStackOperations.java
@@ -3,15 +3,33 @@ package it.unisa.se.calculator.model;
 import static it.unisa.se.calculator.model.ComplexNumber.*;
 
 
+
 public class ArithmeticStackOperations {
     private ComplexNumberStack numberStack;
 
     public ArithmeticStackOperations() {
         numberStack = ComplexNumberStack.getInstance();
     }
+    public void subtract() {
+        ComplexNumber complexNumber1 = numberStack.pop();
+        ComplexNumber complexNumber2 = numberStack.pop();
 
+        ComplexNumber result = ComplexNumber.subtract(complexNumber1,complexNumber2);
+
+        numberStack.push(result);
+
+    }
     public void sum(){
 
+    }
+
+    public void multiply (){
+        ComplexNumber complexNumber1 = numberStack.pop();
+        ComplexNumber complexNumber2 = numberStack.pop();
+
+        ComplexNumber result = ComplexNumber.multiply(complexNumber1,complexNumber2);
+
+        numberStack.push(result);
     }
 
 }
