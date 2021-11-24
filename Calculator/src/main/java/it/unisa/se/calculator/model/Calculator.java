b2a75fcba203bceade48ada389fa9cd9737d0084
pairooo <82327028+pairooo@users.noreply.github.com>
2021-11-24 14:32:06 +0100

    add Test for ArithmeticStackOperations

--- Calculator/src/main/java/it/unisa/se/calculator/model/Calculator.java
+++ Calculator/src/main/java/it/unisa/se/calculator/model/Calculator.java
@@ -1,11 +1,15 @@
 package it.unisa.se.calculator.model;
 
 public class Calculator {
+
     private ComplexNumberStack complexNumberStack;
+
     public Calculator(ComplexNumberStack complexNumberStack) {
         this.complexNumberStack = complexNumberStack;
     }
+
     public String InputDispatcher(String s){
+
         String onlyReal = "[\\+\\- ]?[0-9]+";
         String onlyImaginary = "[ ]?[\\+\\-][0-9]+[ij]";
         String fullComplexNumber = "[\\+\\- ]?[0-9]+[ ]?[\\+\\-][0-9]+[ij]";
