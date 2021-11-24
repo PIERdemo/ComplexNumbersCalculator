33809b97d80683cfd0c822b1180dd6af739885e7
Piorg99 <pio.r2299@gmail.com>
2021-11-24 14:22:43 +0100

    implement InputDispatcher method and TestCalculator class

--- Calculator/src/main/java/it/unisa/se/calculator/model/Calculator.java
+++ Calculator/src/main/java/it/unisa/se/calculator/model/Calculator.java
@@ -5,7 +5,16 @@ public class Calculator {
     public Calculator(ComplexNumberStack complexNumberStack) {
         this.complexNumberStack = complexNumberStack;
     }
-    public void InputDispatcher(String s){
+    public String InputDispatcher(String s){
+        String onlyReal = "[\\+\\- ]?[0-9]+";
+        String onlyImaginary = "[ ]?[\\+\\-][0-9]+[ij]";
+        String fullComplexNumber = "[\\+\\- ]?[0-9]+[ ]?[\\+\\-][0-9]+[ij]";
 
+        if (s.matches(onlyReal) || s.matches(onlyImaginary) || s.matches(fullComplexNumber))
+            return "saveNumber(s)";
+        else
+           return "executeOperation(s)";
     }
+
+
 }
--- Calculator/src/main/java/it/unisa/se/calculator/test/TestCalculator.java
+++ Calculator/src/main/java/it/unisa/se/calculator/test/TestCalculator.java
@@ -1,5 +1,29 @@
 package it.unisa.se.calculator.test;
 
+import it.unisa.se.calculator.model.Calculator;
+import it.unisa.se.calculator.model.ComplexNumberStack;
+import org.junit.Before;
+import org.junit.Test;
+
+
 public class TestCalculator {
+    private Calculator calculator;
+
+    @Before
+    public void setUp(){
+        calculator = new Calculator(ComplexNumberStack.getInstance());
+    }
+
+    @Test
+    public void testInputDispatcher(){
+        calculator.InputDispatcher("1+3i");
+        calculator.InputDispatcher(" 11+11j");
+        calculator.InputDispatcher("1-3i");
+        calculator.InputDispatcher(" -11j");
+        calculator.InputDispatcher("sum");
+        calculator.InputDispatcher("1+3a");
+        calculator.InputDispatcher("+3");
+    }
+
 
 }
