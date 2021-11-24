caf343b4df821191e710af8525463e36c003b331
pairooo <82327028+pairooo@users.noreply.github.com>
2021-11-24 13:10:19 +0100

    implement ArithmeticStackOperations constructor

--- Calculator/src/main/java/it/unisa/se/calculator/model/ArithmeticStackOperations.java
+++ Calculator/src/main/java/it/unisa/se/calculator/model/ArithmeticStackOperations.java
@@ -1,4 +1,12 @@
 package it.unisa.se.calculator.model;
 
 public class ArithmeticStackOperations {
+    private ComplexNumberStack numberStack;
+
+    public ArithmeticStackOperations() {
+        numberStack = ComplexNumberStack.getInstance();
+    }
+
+
+
 }
