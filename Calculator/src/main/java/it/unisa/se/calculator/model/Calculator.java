a9b7130a1e44411e1d5c5fe03be8884cd2b72479
Piorg99 <pio.r2299@gmail.com>
2021-11-24 10:59:48 +0100

    adding Calculator class

---
+++ Calculator/src/main/java/it/unisa/se/calculator/model/Calculator.java
@@ -0,0 +1,11 @@
+package it.unisa.se.calculator.model;
+
+public class Calculator {
+    private ComplexNumberStack complexNumberStack;
+    public Calculator(ComplexNumberStack complexNumberStack) {
+        this.complexNumberStack = complexNumberStack;
+    }
+    public void InputDispatcher(String s){
+
+    }
+}
