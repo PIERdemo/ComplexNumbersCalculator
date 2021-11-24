c6f4021d1080066da40b00cd32e67ad0b0d67b56
Piorg99 <pio.r2299@gmail.com>
2021-11-24 11:06:27 +0100

    adding ComplexNumber class

--- Calculator/src/main/java/it/unisa/se/calculator/model/ComplexNumber.java
+++ Calculator/src/main/java/it/unisa/se/calculator/model/ComplexNumber.java
@@ -1,7 +1,12 @@
 package it.unisa.se.calculator.model;
 
 public class ComplexNumber {
-    public ComplexNumber() {
+    private final Double real;
+    private final Double imaginary;
 
+    public ComplexNumber(Double real, Double imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
     }
+
 }
