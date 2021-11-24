b2322e26f384d462ffc6971adb5fa9d47e3657d0
pairooo <82327028+pairooo@users.noreply.github.com>
2021-11-24 21:16:05 +0100

    add in testCalculator the limit value 0

--- Calculator/src/main/java/it/unisa/se/calculator/model/Calculator.java
+++ Calculator/src/main/java/it/unisa/se/calculator/model/Calculator.java
@@ -8,7 +8,7 @@ public class Calculator {
         this.complexNumberStack = complexNumberStack;
     }
 
-    public void inputDispatcher(String s){
+    public void inputDispatcher(String s) {
 
         String onlyReal = "[\\+\\-]?((([0-9]*).([0-9]+))|([0-9]+))";
         String onlyImaginary = "[\\+|\\-|\\s]?((([0-9]*).([0-9]+))|([0-9]+))[ij]";
@@ -16,25 +16,26 @@ public class Calculator {
 
         if (s.matches(fullComplexNumber))
             saveNumber(s);
-        else if(s.matches(onlyReal))
-            saveNumber(s+"+0j");
-        else if(s.matches(onlyImaginary)) {
-            s=((s.charAt(0)+"").matches("[0-9]"))?("+"+s):s;
+        else if (s.matches(onlyReal))
+            saveNumber(s + "+0j");
+        else if (s.matches(onlyImaginary)) {
+            s = ((s.charAt(0) + "").matches("[0-9]")) ? ("+" + s) : s;
             saveNumber("+0" + s);
         }
     }
 
-    private void saveNumber(String s){
-        if(s.charAt(0) != '-' && s.charAt(0) != '+')
-            s="+"+s;
-        int realSign = 1,imaginarySign = 1;
-        if(s.matches(".*-.*-.*")){
+    private void saveNumber(String s) {
+        if (s.charAt(0) != '-' && s.charAt(0) != '+')
+            s = "+" + s;
+        int realSign = 1, imaginarySign = 1;
+
+        if (s.matches(".*-.*-.*")) {
             realSign = -1;
             imaginarySign = -1;
-        }else{
+        } else {
             int minusPosition = s.indexOf("-");
-            if(minusPosition!=-1){
-                if(minusPosition == 0)
+            if (minusPosition != -1) {
+                if (minusPosition == 0)
                     realSign = -1;
                 else
                     imaginarySign = -1;
@@ -42,10 +43,8 @@ public class Calculator {
         }
         s = s.replaceAll("j", "");
         String[] numbers = s.split("[\\+|\\-]");
-        complexNumberStack.push(new ComplexNumber(realSign*Double.parseDouble(numbers[1]),imaginarySign*Double.parseDouble(numbers[2])));
+        complexNumberStack.push(new ComplexNumber(realSign * Double.parseDouble(numbers[1]), imaginarySign * Double.parseDouble(numbers[2])));
     }
-
-
 
 
 }
