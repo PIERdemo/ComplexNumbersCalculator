13e0c25c98b2cbc4481f05452b3f601c7ffd83f2
Piorg99 <pio.r2299@gmail.com>
2021-11-27 11:32:31 +0100

    adding javadoc for ComplexNumberStack

--- Calculator/src/main/java/it/unisa/se/calculator/model/ComplexNumberStack.java
+++ Calculator/src/main/java/it/unisa/se/calculator/model/ComplexNumberStack.java
@@ -6,18 +6,28 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Stack;
 
-
+/**
+ * The class extends the {@link java.util.Stack} class .
+ * Provides an implementation of a ComplexNumbers stack.
+ *
+ */
 public class ComplexNumberStack extends Stack<ComplexNumber>{
     private static ComplexNumberStack instance= null;
 
+    /**
+     * This method provide an instance of a ComplexNumberStack.
+     */
     public static ComplexNumberStack getInstance(){
         if(instance == null)
             instance = new ComplexNumberStack();
         return instance;
     }
 
-
-
+    /**
+     * Constructor of a complex number.
+     * @param k specifies the int value which will be the length of the list of return values
+     * @return a list of the last k elements(ComplexNumber) of the stack
+     */
     public List<ComplexNumber> topKElements(int k){
 
         List<ComplexNumber> renderedComplexNumbers = new ArrayList<>();
